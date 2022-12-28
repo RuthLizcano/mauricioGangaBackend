ceae8367352483df0622c6ff4b73e02df11e9b43
RuthLizcano <rylizcanop@itc.edu.co>
2022-12-28 13:57:48 -0500

    first commit

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var server_1 = require("./server");
-require("./config/mongoose");
-var server = new server_1.Server;
-server.start();
